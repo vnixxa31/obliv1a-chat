597c57df97291d868931be195e899f0ab9d46287
Violet Nixxa <violet@nixxa.net>
2026-08-26 19:13:31 -0400

    rewording phase 1

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
+  adapter: cloudflare(),
+})
